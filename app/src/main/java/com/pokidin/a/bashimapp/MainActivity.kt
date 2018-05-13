7a8b732e8b06c86cc6df9b617bf94afb8efab053
AlexanderPokidin <a.pokidin@gmail.com>
2018-05-13 13:05:09 +0300

    RecyclerView is done

--- app/src/main/java/com/pokidin/a/bashimapp/MainActivity.kt
+++ app/src/main/java/com/pokidin/a/bashimapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.pokidin.a.bashimapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.pokidin.a.bashimapp.data.SearchRepository
 import com.pokidin.a.bashimapp.data.SearchRepositoryProvider
 import com.pokidin.a.bashimapp.data.SourceOfQuotes
@@ -14,22 +18,32 @@ const val tag = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    val compositeDisposable:CompositeDisposable = CompositeDisposable()
-    val repository:SearchRepository = SearchRepositoryProvider.provideSearchRepository()
+    @BindView(R.id.list)
+    lateinit var listView: RecyclerView
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    val repository: SearchRepository = SearchRepositoryProvider.provideSearchRepository()
+    lateinit var adapter: SourceOfQuotesAdapter
 
     private val list: MutableList<SourceOfQuotes> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        val llm = LinearLayoutManager(this)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        listView.layoutManager = llm
 
         compositeDisposable.add(
-            repository.searchSourceOfQuotes()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.io())
-                    .subscribe({ result -> result.forEach { list.addAll(it) }
-                    Log.d(tag, list.toString())
-                    })
+                repository.searchSourceOfQuotes()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({ result ->
+                            result.forEach { list.addAll(it) }
+                            listView.adapter = SourceOfQuotesAdapter(list)
+                            Log.d(tag, list.toString())
+                        })
         )
     }
 }
